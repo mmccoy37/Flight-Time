572233c52a6ac2ba609871932b761f0b1d0c80ed
Matt <mattmccoy94@gmail.com>
2016-09-24 21:13:54 -0400

    spinner

--- servlet.rb
+++ servlet.rb
@@ -2,7 +2,7 @@
 # @Author: matt
 # @Date:   2016-09-24 11:15:35
 # @Last Modified by:   Matt
-# @Last Modified time: 2016-09-24 19:04:39
+# @Last Modified time: 2016-09-24 20:24:15
 require 'sinatra/base'
 require 'net/http'
 require 'json'
@@ -11,7 +11,7 @@ class FlightServlet < Sinatra::Base
     attr_accessor :name, :key, :url
     url = "https://demo30-test.apigee.net/v1/hack/tsa"
     key = "FQFMhNJmXqB34vRNk4THrnT9RiRnLiUG"
-    get "/home" do
+    get "/" do
         erb :index
     end
 
